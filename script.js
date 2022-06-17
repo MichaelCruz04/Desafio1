67751ec63557c5a807dc372ba41b1d27e42b6383
Luiz Felipe <luizftp25@gmail.com>
2022-06-16 23:35:20 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao1 = document.getElementById("botao1");
+const botao2 = document.getElementById("botao2");
+const resultado = document.getElementById("resultado")
+
+function insert(){
+  console.log(botao1.value)
+n1 = botao1
+n2 = botao2
+}
+
+const somar = botao1 * botao2
